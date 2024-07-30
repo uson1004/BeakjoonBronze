e3ede06f9dbf54d861f0f41bb04de58df938d7d7
윤유섭 <166105837+uson1004@users.noreply.github.com>
2024-07-30 14:42:11 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/24086. 身長 （Height）/身長 （Height）.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp;
+    
+    scanf("%d %d", &a, &b);
+    
+    if ( a < b ) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    printf("%d", a - b);
+}
